4aaab4473d20a6b43973f012a54d571272e4e1bc
John Flesch <john@fles.ch>
2016-07-09 21:47:29 -0500

    Adds .editorconfig and uses 4 spaces instead of 2

--- bin/cli.js
+++ bin/cli.js
@@ -3,10 +3,10 @@
 'use strict';
 
 const argv = require('yargs')
-  .command(require('./cmd/search'))
-  .demand(1)
-  .alias('v', 'version')
-  .version(() => require('../package').version)
-  .alias('h', 'help')
-  .help('help')
-  .argv;
+    .command(require('./cmd/search'))
+    .demand(1)
+    .alias('v', 'version')
+    .version(() => require('../package').version)
+    .alias('h', 'help')
+    .help('help')
+    .argv;
--- bin/cmd/search.js
+++ bin/cmd/search.js
@@ -9,82 +9,82 @@ const truncate = require('truncate');
 exports.command = 'search [packages...]';
 exports.desc = 'Search npms.io for packages matching the search terms.';
 exports.builder = {
-  f: {
-    alias: 'from',
-    describe: 'The offset in which to start searching from.',
-    default: 0
-  },
-  s: {
-    alias: 'size',
-    describe: 'The total number of results to return.',
-    default: 10
-  },
-  o: {
-    alias: 'output',
-    describe: 'Format the results in a table or as JSON.',
-    default: 'table'
-  },
-  scoreEffect: {
-    describe: 'The effect that the module scores have for the final search score.',
-  },
-  qualityWeight: {
-    describe: 'The weight that the quality has for the each module score.'
-  },
-  popularityWeight: {
-    describe: 'The weight that the popularity has for each module score.'
-  },
-  maintenanceWeight: {
-    describe: 'The weight that the maintenance has for each module score.'
-  }
+    f: {
+        alias: 'from',
+        describe: 'The offset in which to start searching from.',
+        default: 0
+    },
+    s: {
+        alias: 'size',
+        describe: 'The total number of results to return.',
+        default: 10
+    },
+    o: {
+        alias: 'output',
+        describe: 'Format the results in a table or as JSON.',
+        default: 'table'
+    },
+    scoreEffect: {
+        describe: 'The effect that the module scores have for the final search score.',
+    },
+    qualityWeight: {
+        describe: 'The weight that the quality has for the each module score.'
+    },
+    popularityWeight: {
+        describe: 'The weight that the popularity has for each module score.'
+    },
+    maintenanceWeight: {
+        describe: 'The weight that the maintenance has for each module score.'
+    }
 };
 
 exports.handler = (argv) => {
 
-  got('https://api.npms.io/search', {
-    json: true,
-    query: JSON.parse(JSON.stringify({
-      term: argv.packages.join('+'),
-      from: argv.from,
-      size: argv.size,
-      scoreEffect: argv.scoreEffect,
-      qualityWeight: argv.qualityWeight,
-      popularityWeight: argv.popularityWeight,
-      maintenanceWeight: argv.maintenanceWeight
-    }))
-  }).then(res => {
+    got('https://api.npms.io/search', {
+        json: true,
+        query: JSON.parse(JSON.stringify({
+            term: argv.packages.join('+'),
+            from: argv.from,
+            size: argv.size,
+            scoreEffect: argv.scoreEffect,
+            qualityWeight: argv.qualityWeight,
+            popularityWeight: argv.popularityWeight,
+            maintenanceWeight: argv.maintenanceWeight
+        }))
+    }).then(res => {
 
-    if (!res.body.results.length) {
-      console.log(chalk.red(`No matches found for: "${chalk.white.bold(argv.packages.join('+'))}"`));
-      return false;
-    }
+        if (!res.body.results.length) {
+            console.log(chalk.red(`No matches found for: "${chalk.white.bold(argv.packages.join('+'))}"`));
+            return false;
+        }
 
-    if (argv.output === 'table') {
+        if (argv.output === 'table') {
 
-      const table = new Table({ head:['Package', 'Quality', 'Popularity', 'Maintenance', 'Score'] });
+            const table = new Table({ head:['Package', 'Quality', 'Popularity', 'Maintenance', 'Score'] });
 
-      table.push.apply(table, res.body.results.map(item => {
+            table.push.apply(table, res.body.results.map(item => {
 
-        let module = item.module
-        let pkg = [
-          `${chalk.bold(module.name)} • ${chalk.dim(module.links.repository || module.links.npm)}`,
-          chalk.gray(truncate(module.description, 80, { ellipsis:'...' })),
-          chalk.dim(`updated ${moment(module.date).fromNow()} by ${module.publisher.username}`)
-        ].join('\n');
+                let module = item.module
+                let pkg = [
+                    `${chalk.bold(module.name)} • ${chalk.dim(module.links.repository || module.links.npm)}`,
+                    chalk.gray(truncate(module.description, 80, { ellipsis:'...' })),
+                    chalk.dim(`updated ${moment(module.date).fromNow()} by ${module.publisher.username}`)
+                ].join('\n');
 
-        let score = ['quality', 'popularity', 'maintenance'].map(score => {
-          return { hAlign:'center', vAlign:'center', content:Math.round(item.score.detail[score] * 100) };
-        }).concat([{ hAlign:'center', vAlign:'center', content:chalk.green(Math.round(item.score.final * 100)) }]);
+                let score = ['quality', 'popularity', 'maintenance'].map(score => {
+                    return { hAlign:'center', vAlign:'center', content:Math.round(item.score.detail[score] * 100) };
+                }).concat([{ hAlign:'center', vAlign:'center', content:chalk.green(Math.round(item.score.final * 100)) }]);
 
-        return [pkg].concat(score);
+                return [pkg].concat(score);
 
-      }));
+            }));
 
-      console.log(table.toString());
+            console.log(table.toString());
 
-    } else {
-      console.log(res.body.results);
-    }
+        } else {
+            console.log(res.body.results);
+        }
 
-  }).catch(err => console.log(err));
+    }).catch(err => console.log(err));
 
 };
