f1f33b33b1f9f32c82ce275d1bc524f53ec4a525
John Flesch <john@fles.ch>
2016-07-09 22:05:28 -0500

    Adds update-notifier

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,11 @@
 
 'use strict';
 
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
+
+updateNotifier({ pkg }).notify();
+
 const argv = require('yargs')
     .command(require('./cmd/search'))
     .demand(1)
