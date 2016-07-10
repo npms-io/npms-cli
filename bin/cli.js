1e3159c28ab52c56bb8417e242db36550a30c63c
John Flesch <john@fles.ch>
2016-07-09 21:13:26 -0500

    Separates the "search" subcommand into its own file
    This also integrates @mikeerickson's fixes for #5 (PR #6) to support the latest API response format.

--- bin/cli.js
+++ bin/cli.js
@@ -1,60 +1,12 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 'use strict';
 
-const got = require('got');
-const Table = require('cli-table2');
-const chalk = require('chalk');
-const moment = require('moment');
-const truncate = require('truncate');
-
 const argv = require('yargs')
+  .command(require('./cmd/search'))
+  .demand(1)
   .alias('v', 'version')
   .version(() => require('../package').version)
-  .command('search', 'Search npms.io for packages matching the search terms.', (yargs) => {
-      return yargs.option('n', {
-        alias: 'number',
-        describe: 'Limit the number of search results returned.',
-        default: 10
-      })
-    },
-    (argv) => {
-
-      got('https://api.npms.io/search', {
-        json: true,
-        query: {
-          term: argv._.slice(1).join('+'),
-          size: argv.number
-        }
-      }).then(res => {
-
-        const table = new Table({ head:['Package', 'Quality', 'Popularity', 'Maintenance', 'Score'] });
-
-        table.push.apply(table, res.body.results.map(item => {
-
-          let pkg = [
-            `${chalk.bold(item.name)} • ${chalk.dim(item.links.repository || item.links.npm)}`,
-            chalk.gray(truncate(item.description, 80, { ellipsis:'...' })),
-            chalk.dim(`updated ${moment(item.date).fromNow()} by ${item.publisher.username}`)
-          ].join('\n');
-
-          let score = ['quality', 'popularity', 'maintenance'].map(score => {
-            return { hAlign:'center', vAlign:'center', content:Math.round(item.score.detail[score] * 100) };
-          }).concat([{ hAlign:'center', vAlign:'center', content:chalk.green(Math.round(item.score.final * 100)) }]);
-
-          return [pkg].concat(score);
-
-        }));
-
-        console.log(table.toString());
-
-      }).catch(err => {
-        // console.error(err);
-      });
-    }
-  )
-  .usage('$0 search [-n|--number 10] search terms...')
-  .example('$0 search -n 15 react')
   .alias('h', 'help')
   .help('help')
-  .argv
+  .argv;
