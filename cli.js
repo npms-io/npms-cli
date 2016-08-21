ea6dc975105c346375e68ed540f66e14495d22f6
Andre Cruz <andremiguelcruz@msn.com>
2016-08-21 12:49:04 +0100

    Unnecessary module.exports.

--- cli.js
+++ cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+/* eslint no-unused-expressions:0 */
+
 'use strict';
 
 const yargs = require('yargs');
@@ -8,15 +10,15 @@ const pkg = require('./package.json');
 
 updateNotifier({ pkg }).notify();
 
-module.exports = yargs
-    .strict()
-    .wrap(Math.min(120, yargs.terminalWidth()))
-    .demand(1)
-    .command(require('./cmd/search'))
-    .command(require('./cmd/info'))
-    .command(require('./cmd/open'))
-    .alias('v', 'version')
-    .version(() => pkg.version)
-    .alias('h', 'help')
-    .help('help')
-    .argv;
+yargs
+.strict()
+.wrap(Math.min(120, yargs.terminalWidth()))
+.alias('v', 'version')
+.version(() => pkg.version)
+.alias('h', 'help')
+.help('help')
+.demand(1)
+.command(require('./cmd/search'))
+.command(require('./cmd/info'))
+.command(require('./cmd/open'))
+.argv;
