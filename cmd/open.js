5f9d8560d81a98c303ff0c78afc406b2d66e3b99
Andre Cruz <andremiguelcruz@msn.com>
2016-09-10 19:07:32 +0100

    Add color option to open command.

--- cmd/open.js
+++ cmd/open.js
@@ -6,6 +6,16 @@ const handleError = require('./util/handleError');
 
 exports.command = 'open <package>';
 exports.describe = 'Opens the package in your browser.';
+exports.builder = (yargs) =>
+    yargs
+    .strict()
+    .options({
+        color: {
+            describe: 'Allows disabling or enabling colored output',
+            type: 'boolean',
+            default: true,
+        },
+    });
 
 exports.handler = (argv) => {
     got(`https://api.npms.io/module/${encodeURIComponent(argv.package)}`, { json: true })
